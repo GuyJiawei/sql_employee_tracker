49d5520e8494abb98fcea9078ddbea10a60767e0
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-02-15 17:40:50 +1100

    initialised and added base server code

---
+++ server.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql2");
+const inquirer = require('inquirer');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
