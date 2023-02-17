f55b6fc9e8b86736b4445675400a73179cd1a7cd
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-02-18 09:33:14 +1100

    added managerID to add employee function

--- db/schema.sql
+++ db/schema.sql
@@ -24,8 +24,7 @@ CREATE TABLE employee (
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    --FOREIGN KEY (manager_id)
-    --REFERENCES employee(id)
-    ON DELETE SET NULL
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(employee_id)
 );
